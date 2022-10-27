f20a66c2dc89cf3418c67ef43e2f959cbcd0ec5d
Bilal Ahmed <bilalahmedpak.03@gmail.com>
2022-10-27 21:13:48 +0400

    delete api is added

--- blog/urls.py
+++ blog/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('posts/', include('post.urls'))
+    path('posts/', include('post.urls')),
+    path('/', include('accounts.urls')),
+
 ]
